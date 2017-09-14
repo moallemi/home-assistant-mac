951c8f2339349dae19619aabf22afdbdfa2bf8e9
Reza Moallemi <rm.allmails@gmail.com>
2017-09-14 17:32:13 +0430

    Initial Commit

---
+++ Home Assistant/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Home Assistant
+//
+//  Created by Reza Moallemi on 9/14/17.
+//  Copyright © 2017 Reza Moallemi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Home Assistant/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Home Assistant
+//
+//  Created by Reza Moallemi on 9/14/17.
+//  Copyright © 2017 Reza Moallemi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
