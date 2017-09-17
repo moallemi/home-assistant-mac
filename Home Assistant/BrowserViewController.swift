f8b5bc6b163756c3f6676b50c0bdd9784722e0d1
Reza Moallemi <rm.allmails@gmail.com>
2017-09-17 20:13:44 +0430

    Add loading progress view

--- Home Assistant/BrowserViewController.swift
+++ Home Assistant/BrowserViewController.swift
@@ -12,13 +12,27 @@ import WebKit
 class BrowserViewController: NSViewController, WKNavigationDelegate {
 
     @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var progressView: NSProgressIndicator!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         webView.navigationDelegate = self
+        webView.addObserver(self, forKeyPath: "loading", options: .new, context: nil)
+        webView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
         
         webView.load(URLRequest(url: URL(string: PreferenceManager.sharedInstance.defaultAddress)!))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        progressView.doubleValue = 0.0
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if (keyPath == "estimatedProgress") {
+            progressView.isHidden = webView.estimatedProgress == 1.0
+            progressView.doubleValue = webView.estimatedProgress
+        }
     }
     
     func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
